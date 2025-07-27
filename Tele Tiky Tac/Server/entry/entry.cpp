2960c3ce983d2628526e137e71bdd4e3a711e34f
Penguu <jamalissproute2@gmail.com>
2025-07-27 14:01:41 +0300

    add

--- Tele Tiky Tac/Server/entry/entry.cpp
+++ Tele Tiky Tac/Server/entry/entry.cpp
@@ -8,6 +8,8 @@
 #include <thread>
 #include <iostream>
 
+#pragma comment(lib, "ws2_32.lib")
+
 class Server
 {
 public:
@@ -21,418 +23,141 @@ public:
 	{
 		SOCKET socket;
 		size_t id;
-		std::string name;
 		bool connected;
 	};
 
 private:
 	std::vector<Client> clients;
-	SOCKET server;
-	std::thread serverThread;
-
+	SOCKET serverSocket;
 	bool running;
-	Settings settings;
-
-	void HandleConnection(SOCKET connection)
-	{
-		Client newClient;
-		newClient.socket = connection;
-		newClient.name = GetSocketRemoteName(connection);
-		newClient.id = clients.size() + 1;
-		newClient.connected = true;
-		clients.push_back(newClient);
-
-		std::cout << "New connection " << newClient.name << "\n";
-
-		ClientHandler(newClient.id);
-	}
-
-	void ClientHandler(size_t clientId)
-	{
-		std::thread([this, clientId]()
-			{
-				while (running)
-				{
-					Client* client = FindClientById(clientId);
-					if (client == nullptr || !client->connected)
-					{
-						break;
-					}
-
-					char buffer;
-					int result = recv(client->socket, &buffer, 1, MSG_PEEK);
-
-					if (result == 0 || (result == SOCKET_ERROR && WSAGetLastError() != WSAEWOULDBLOCK))
-					{
-						RemoveClient(*client, true);
-						break;
-					}
-
-					Sleep(1300);
-				}
-			}
-		).detach();
-	}
 
 public:
-	Server(Settings newSettings)
-	{
-		running = false;
-		server = INVALID_SOCKET;
-		settings = newSettings;
-		serverThread = std::thread([this]() { Start(); });
-	}
-
-	Server()
-	{
-		running = false;
-		server = INVALID_SOCKET;
-	}
-
-	~Server()
-	{
-		Stop();
-		if (serverThread.joinable()) serverThread.join();
-	}
-
-	bool Start()
+	Server(Settings settings)
 	{
 		WSADATA wsaData;
-		int status = WSAStartup(MAKEWORD(2, 2), &wsaData);
-		if (status != 0)
-		{
-			throw std::runtime_error("WSAStartup failed with status code " + std::to_string(status));
-			return false;
-		}
+		WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-		struct addrinfo* result = nullptr;
-		struct addrinfo hints;
-
-		ZeroMemory(&hints, sizeof(hints));
+		addrinfo hints{}, * result = nullptr;
 		hints.ai_family = AF_INET;
 		hints.ai_socktype = SOCK_STREAM;
 		hints.ai_protocol = IPPROTO_TCP;
 		hints.ai_flags = AI_PASSIVE;
+		getaddrinfo(NULL, settings.port.c_str(), &hints, &result);
 
-		status = getaddrinfo(0, settings.port.c_str(), &hints, &result);
-		if (status != 0)
-		{
-			throw std::runtime_error("getaddrinfo failed with status code " + std::to_string(status));
-			WSACleanup();
-			return false;
-		}
-
-		server = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-		if (server == INVALID_SOCKET)
-		{
-			throw std::runtime_error("socket failed with status code " + std::to_string(WSAGetLastError()));
-			freeaddrinfo(result);
-			WSACleanup();
-			return false;
-		}
-
-		status = bind(server, result->ai_addr, (int)result->ai_addrlen);
-		if (status != 0)
-		{
-			throw std::runtime_error("bind failed with status code " + std::to_string(WSAGetLastError()));
-			freeaddrinfo(result);
-			closesocket(server);
-			WSACleanup();
-			return false;
-		}
-
-		freeaddrinfo(result);
-
-		status = listen(server, SOMAXCONN);
-		if (status == SOCKET_ERROR)
-		{
-			throw std::runtime_error("listen failed with error: " + std::to_string(WSAGetLastError()));
-			closesocket(server);
-			WSACleanup();
-			return false;
-		}
-
-		if (settings.debugMessages)
-		{
-			std::cout << "Server is listening for incoming connections.\n";
-		}
+		serverSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+		bind(serverSocket, result->ai_addr, (int)result->ai_addrlen);
+		listen(serverSocket, SOMAXCONN);
 
 		running = true;
-
-		while (running)
-		{
-			SOCKET clientSocket = accept(server, 0, 0);
-			if (clientSocket == INVALID_SOCKET)
-			{
-				continue;
-			}
-
-			HandleConnection(clientSocket);
-		}
-
-		return true;
+		std::thread(&Server::Run, this).detach();
 	}
 
-	bool Stop()
+	~Server()
 	{
 		running = false;
-		KickAll();
-		closesocket(server);
+		closesocket(serverSocket);
 		WSACleanup();
-		return true;
 	}
 
-	void RemoveClient(Client client, bool serverMessage = false)
+	void Run()
 	{
-		for (auto it = clients.begin(); it != clients.end(); ++it)
+		while (running)
 		{
-			if (it->id == client.id)
+			SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+			if (clientSocket != INVALID_SOCKET)
 			{
-				if (settings.debugMessages || serverMessage)
+				Client c{ clientSocket, clients.size() + 1, true };
+				clients.push_back(c);
+				if (clients.size() == 1)
 				{
-					std::cout << "Client disconnected " << client.name << "\n";
+					Send(c, "WaitingForPlayer");
 				}
-
-				shutdown(it->socket, SD_BOTH);
-				closesocket(it->socket);
-				clients.erase(it);
-				break;
+				else if (clients.size() == 2)
+				{
+					std::thread(&Server::GameLoop, this, clients[0], clients[1]).detach();
+				}
 			}
 		}
 	}
 
-	void KickAll()
+	void Send(Client client, std::string msg)
 	{
-		while (!clients.empty())
-		{
-			RemoveClient(clients.back());
-		}
+		send(client.socket, msg.c_str(), (int)msg.size(), 0);
 	}
 
-	void Broadcast(std::string message)
+	std::string Receive(Client client)
 	{
-		for (auto& client : clients)
-		{
-			Send(client, message);
-		}
+		char buffer[128] = {};
+		int len = recv(client.socket, buffer, sizeof(buffer), 0);
+		return (len > 0) ? std::string(buffer, len) : "";
 	}
 
-	bool Send(Client client, std::string message)
+	void GameLoop(Client p1, Client p2)
 	{
-		if (!client.connected)
-		{
-			return true;
-		}
+		char board[3][3];
+		memset(board, ' ', sizeof(board));
+		size_t turn = 1;
+		int moves = 0;
 
-		size_t sizeSent = 0;
-		size_t messageSize = message.size() + 1;
-
-		while (sizeSent < messageSize)
-		{
-			int sent = send(client.socket, message.c_str(), messageSize, 0);
-			if (sent == SOCKET_ERROR)
+		auto DrawBoard = [&]() -> std::string
 			{
-				return false;
-			}
-			sizeSent += sent;
-		}
-		return true;
-	}
-
-	std::string Receive(Client& client)
-	{
-		if (!client.connected)
-		{
-			return "";
-		}
-
-		std::string result;
-		char buffer[1024];
+				std::string s;
+				for (int i = 0; i < 3; ++i)
+				{
+					for (int j = 0; j < 3; ++j)
+						s += board[i][j];
+					s += '\n';
+				}
+				return s;
+			};
 
 		while (true)
 		{
-			int received = recv(client.socket, buffer, sizeof(buffer), 0);
-			if (received == SOCKET_ERROR || received == 0)
+			Client& current = (turn == 1) ? p1 : p2;
+			Client& other = (turn == 1) ? p2 : p1;
+			Send(current, "YourMove\n" + DrawBoard());
+			Send(other, "Waiting\n" + DrawBoard());
+			std::string move = Receive(current);
+			if (move.length() < 2) continue;
+			int r = move[0] - '0';
+			int c = move[1] - '0';
+			if (r < 0 || r > 2 || c < 0 || c > 2 || board[r][c] != ' ') continue;
+			board[r][c] = (turn == 1) ? 'X' : 'O';
+			moves++;
+
+			for (int i = 0; i < 3; i++)
 			{
-				break;
+				if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ' ||
+					board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+				{
+					Send(current, "Win\n" + DrawBoard());
+					Send(other, "Lose\n" + DrawBoard());
+					return;
+				}
 			}
-
-			result.append(buffer, received);
-
-			if (received < sizeof(buffer))
+			if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ' ||
+				board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ')
 			{
-				break;
+				Send(current, "Win\n" + DrawBoard());
+				Send(other, "Lose\n" + DrawBoard());
+				return;
 			}
-		}
-
-		return result;
-	}
-
-	std::string Communicate(Client client, std::string message)
-	{
-		Send(client, message);
-		return Receive(client);
-	}
-
-	bool IsClientConnected(Client client)
-	{
-		if (client.connected == false)
-		{
-			return false;
-		}
-		if (client.id > clients.size()) // best one
-		{
-			return false;
-		}
-		Client* found = FindClientById(client.id);
-		if (found == nullptr)
-		{
-			return false;
-		}
-		return found->connected;
-	}
-
-	SOCKET GetServer()
-	{
-		return server;
-	}
-	Client* FindClientById(size_t id)
-	{
-		for (auto& client : clients)
-		{
-			if (client.id == id)
+			if (moves == 9)
 			{
-				return &client;
+				Send(p1, "Draw\n" + DrawBoard());
+				Send(p2, "Draw\n" + DrawBoard());
+				return;
 			}
+			turn = (turn == 1) ? 2 : 1;
 		}
-		return nullptr;
-	}
-	std::vector<Client> GetClients()
-	{
-		return clients;
-	}
-	Client GetClient(size_t iteratorNumber)
-	{
-		if (iteratorNumber >= clients.size())
-		{
-			return {};
-		}
-		return clients[iteratorNumber];
-	}
-
-	static std::string GetSocketRemoteName(SOCKET s)
-	{
-		sockaddr_in addr;
-		int addrSize = sizeof(addr);
-		if (getpeername(s, (sockaddr*)&addr, &addrSize) == 0)
-		{
-			char ipStr[INET_ADDRSTRLEN] = { 0 };
-			inet_ntop(AF_INET, &(addr.sin_addr), ipStr, INET_ADDRSTRLEN);
-
-			unsigned short port = ntohs(addr.sin_port);
-
-			return std::string(ipStr) + ":" + std::to_string(port);
-		}
-		return "Unknown";
 	}
 };
 
-int HasPlayerLeft(Server& server, Server::Client& player1, Server::Client& player2)
-{
-	int status = 0;
-
-	if (server.IsClientConnected(player1))
-	{
-		status++;
-	}
-	if (server.IsClientConnected(player2))
-	{
-		status++;
-	}
-	return status;
-}
-
-std::string player1Query;
-std::string player2Query;
-
-void ReceiveMsgThread(Server& server, Server::Client& player1, Server::Client& player2)
-{
-	std::thread([&server, &player1]()
-		{
-			while (true)
-			{
-				player1Query = server.Receive(player1);
-			}
-		}
-	).detach();
-	std::thread([&server, &player2]()
-		{
-			while (true)
-			{
-				player1Query = server.Receive(player2);
-			}
-		}
-	).detach();
-}
-
 int main()
 {
-	// Initializing our server
-	Server::Settings settings;
-	settings.port = "3709";
-	settings.debugMessages = true;
-	Server server(settings);
-
-	// Server loop
-	bool hasGameStarted = false;
-	bool hasRoundStarted = false;
-	bool hasToldClientToStart = false;
-
-	Server::Client player1;
-	Server::Client player2;
-	ReceiveMsgThread(server, player1, player2);
-	while (true)
-	{
-		player1 = server.GetClient(0);
-		player2 = server.GetClient(1);
-		if (!player1.connected || !player2.connected)
-		{
-			server.Send(player1, "WaitScreen");
-			hasToldClientToStart = false;
-			Sleep(1000);
-		}
-		else if (player1.connected && player2.connected)
-		{
-			for (int waitTime = 3; waitTime > 0; waitTime--)
-			{
-				int hasPlayerLeft = HasPlayerLeft(server, player1, player2);
-				if (hasPlayerLeft != 2)
-				{
-					break;
-				}
-				else
-				{
-					server.Broadcast("StartIn" + std::to_string(waitTime));
-					Sleep(1000);
-				}
-			}
-			//while (true)
-			//{
-			//	if (!hasToldClientToStart)
-			//	{
-			//		server.Broadcast("Start");
-			//		hasToldClientToStart = true;
-			//	}
-			//	int hasPlayerLeft = HasPlayerLeft(server, player1, player2);
-			//	if (hasPlayerLeft != 2)
-			//	{
-			//		break;
-			//	}
-			//	else
-			//	{
-			//		// Game using receive query
-			//	}
-			//}
-		}
-	}
+	Server::Settings s;
+	s.port = "7777";
+	s.debugMessages = true;
+	Server server(s);
+	while (true) Sleep(1000);
 }
--- Tele Tiky Tac/Tele Tiky Tac/entry/entry.cpp
+++ Tele Tiky Tac/Tele Tiky Tac/entry/entry.cpp
@@ -1,120 +1,258 @@
-#define NOGDICAPMASKS     // CC_*, LC_*, PC_*, CP_*, TC_*, RC_
-#define NOVIRTUALKEYCODES // VK_*
-#define NOWINMESSAGES     // WM_*, EM_*, LB_*, CB_*
-#define NOWINSTYLES       // WS_*, CS_*, ES_*, LBS_*, SBS_*, CBS_*
-#define NOSYSMETRICS      // SM_*
-#define NOMENUS           // MF_*
-#define NOICONS           // IDI_*
-#define NOKEYSTATES       // MK_*
-#define NOSYSCOMMANDS     // SC_*
-#define NORASTEROPS       // Binary and Tertiary raster ops
-#define NOSHOWWINDOW      // SW_*
-#define OEMRESOURCE       // OEM Resource values
-#define NOATOM            // Atom Manager routines
-#define NOCLIPBOARD       // Clipboard routines
-#define NOCOLOR           // Screen colors
-#define NOCTLMGR          // Control and Dialog routines
-#define NODRAWTEXT        // DrawText() and DT_*
-#define NOGDI             // All GDI defines and routines
-#define NOKERNEL          // All KERNEL defines and routines
-#define NOUSER            // All USER defines and routines
-//#define NONLS             // All NLS defines and routines
-#define NOMB              // MB_* and MessageBox()
-#define NOMEMMGR          // GMEM_*, LMEM_*, GHND, LHND, associated routines
-#define NOMETAFILE        // typedef METAFILEPICT
-#define NOMINMAX          // Macros min(a,b) and max(a,b)
-#define NOMSG             // typedef MSG and associated routines
-#define NOOPENFILE        // OpenFile(), OemToAnsi, AnsiToOem, and OF_*
-#define NOSCROLL          // SB_* and scrolling routines
-#define NOSERVICE         // All Service Controller routines, SERVICE_ equates, etc.
-#define NOSOUND           // Sound driver routines
-#define NOTEXTMETRIC      // typedef TEXTMETRIC and associated routines
-#define NOWH              // SetWindowsHook and WH_*
-#define NOWINOFFSETS      // GWL_*, GCL_*, associated routines
-#define NOCOMM            // COMM driver routines
-#define NOKANJI           // Kanji support stuff.
-#define NOHELP            // Help engine interface.
-#define NOPROFILER        // Profiler interface.
-#define NODEFERWINDOWPOS  // DeferWindowPos routines
-#define NOMCX             // Modem Configuration Extensions
+#define NOGDICAPMASKS
+#define NOVIRTUALKEYCODES
+#define NOWINMESSAGES
+#define NOWINSTYLES
+#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOKEYSTATES
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define NOSHOWWINDOW
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOGDI
+#define NOKERNEL
+#define NOUSER
+#define NOMB
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOMSG
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOWINOFFSETS
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+
 #include "raylib.h"
 #include "network/network.h"
+#include <thread>
+#include <string>
 
-void DrawCenteredText(std::string text, int fontSize, Color color = BLACK)
+SOCKET ConnectToServer(const char* ip, const char* port)
 {
-	int textWidth = MeasureText(text.c_str(), fontSize);
-	int screenWidth = GetScreenWidth();
-	int screenHeight = GetScreenHeight();
-
-	int x = (screenWidth - textWidth) / 2;
-	int y = (screenHeight - fontSize) / 2;
-
-	DrawText(text.c_str(), x, y, fontSize, BLACK);
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+	addrinfo hints{}, * res;
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	getaddrinfo(ip, port, &hints, &res);
+	SOCKET s = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (connect(s, res->ai_addr, (int)res->ai_addrlen) != 0)
+	{
+		closesocket(s);
+		s = INVALID_SOCKET;
+	}
+	freeaddrinfo(res);
+	return s;
 }
 
-std::string queryReceive;
-void ReceiveMsgThread(Client& client)
+std::string latestMsg = "";
+void ReceiveThread(SOCKET sock)
 {
-	std::thread([&client]()
-		{
-			while (true)
-			{
-				queryReceive = client.Receive();
-			}
-		}
-	).detach();
+	char buffer[512];
+	while (true)
+	{
+		int len = recv(sock, buffer, sizeof(buffer) - 1, 0);
+		if (len <= 0) break;
+		buffer[len] = 0;
+		latestMsg = buffer;
+	}
 }
 
 int main()
 {
-	// Connecting to the sever
-	Client::Settings settings;
-	settings.port = "3709";
-	settings.ip = "127.0.0.1";
-	settings.debugMessages = false;
-	settings.alwaysConnect = true;
-	settings.secondsTimeout = -1;
-	Client client(settings);
-	ReceiveMsgThread(client);
-
-	// Setting raylib
-	InitWindow(900, 700, "Tele Tiky Tac");
+	InitWindow(600, 200, "Connect to Server");
 	SetTargetFPS(60);
 
-	// Game loop
+	char ip[64] = "127.0.0.1";
+	char port[16] = "7777";
+	int ipLen = (int)strlen(ip);
+	int portLen = (int)strlen(port);
+	bool editingIp = true;
+	bool connectRequested = false;
+	bool connectFailed = false;
+	SOCKET s = INVALID_SOCKET;
+
+	while (!WindowShouldClose() && !connectRequested)
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawText("Enter IP:", 50, 40, 20, DARKGRAY);
+		DrawRectangle(150, 35, 300, 30, LIGHTGRAY);
+		DrawText(ip, 155, 40, 20, BLACK);
+
+		DrawText("Enter Port:", 50, 90, 20, DARKGRAY);
+		DrawRectangle(150, 85, 300, 30, LIGHTGRAY);
+		DrawText(port, 155, 90, 20, BLACK);
+
+		DrawRectangle(470, 35, 80, 80, SKYBLUE);
+		DrawText("Connect", 485, 70, 20, BLACK);
+
+		if (connectFailed)
+		{
+			DrawText("Failed to connect!", 150, 130, 20, RED);
+		}
+
+		if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+		{
+			Vector2 mouse = GetMousePosition();
+			if (mouse.x >= 470 && mouse.x <= 550 && mouse.y >= 35 && mouse.y <= 115)
+			{
+				s = ConnectToServer(ip, port);
+				if (s != INVALID_SOCKET)
+				{
+					connectRequested = true;
+				}
+				else
+				{
+					connectFailed = true;
+				}
+			}
+			else if (mouse.y >= 35 && mouse.y <= 65)
+			{
+				editingIp = true;
+			}
+			else if (mouse.y >= 85 && mouse.y <= 115)
+			{
+				editingIp = false;
+			}
+		}
+
+		int key = GetCharPressed();
+		while (key > 0)
+		{
+			if (key >= 32 && key <= 126)
+			{
+				if (editingIp && ipLen < 63)
+				{
+					ip[ipLen++] = (char)key;
+					ip[ipLen] = 0;
+				}
+				else if (!editingIp && portLen < 15)
+				{
+					port[portLen++] = (char)key;
+					port[portLen] = 0;
+				}
+			}
+			key = GetCharPressed();
+		}
+
+		if (IsKeyPressed(KEY_BACKSPACE))
+		{
+			if (editingIp && ipLen > 0)
+				ip[--ipLen] = 0;
+			else if (!editingIp && portLen > 0)
+				port[--portLen] = 0;
+		}
+
+		EndDrawing();
+	}
+
+	if (!connectRequested)
+	{
+		CloseWindow();
+		return 1;
+	}
+
+	CloseWindow();
+
+	InitWindow(600, 600, "Tic Tac Toe");
+	SetTargetFPS(60);
+
+	std::thread(ReceiveThread, s).detach();
+
+	char board[3][3] = {};
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 
-		ClearBackground(RAYWHITE);
-		if (!client.IsConnected())
+		if (latestMsg.find("YourMove") != std::string::npos)
+			ClearBackground(Color{ 200, 255, 200, 255 }); // light green
+		else if (latestMsg.find("Waiting") != std::string::npos)
+			ClearBackground(Color{ 255, 200, 200, 255 }); // light red
+		else
+			ClearBackground(RAYWHITE);
+
+		if (latestMsg.find("WaitingForPlayer") != std::string::npos)
 		{
-			DrawCenteredText("Server isn't running", 32);
+			Vector2 sz = MeasureTextEx(GetFontDefault(), "Waiting for player...", 30, 1);
+			DrawText("Waiting for player...", (int)((600 - sz.x) / 2), (int)((600 - sz.y) / 2), 30, DARKGRAY);
+		}
+		else if (latestMsg.find("Win") != std::string::npos)
+		{
+			Vector2 sz = MeasureTextEx(GetFontDefault(), "You Win!", 40, 1);
+			DrawText("You Win!", (int)((600 - sz.x) / 2), (int)((600 - sz.y) / 2), 40, GREEN);
+		}
+		else if (latestMsg.find("Lose") != std::string::npos)
+		{
+			Vector2 sz = MeasureTextEx(GetFontDefault(), "You Lose!", 40, 1);
+			DrawText("You Lose!", (int)((600 - sz.x) / 2), (int)((600 - sz.y) / 2), 40, RED);
+		}
+		else if (latestMsg.find("Draw") != std::string::npos)
+		{
+			Vector2 sz = MeasureTextEx(GetFontDefault(), "Draw!", 40, 1);
+			DrawText("Draw!", (int)((600 - sz.x) / 2), (int)((600 - sz.y) / 2), 40, GRAY);
 		}
 		else
 		{
-			if (queryReceive.find("WaitScreen") != std::string::npos) // idk why yet
+			DrawLine(200, 0, 200, 600, BLACK);
+			DrawLine(400, 0, 400, 600, BLACK);
+			DrawLine(0, 200, 600, 200, BLACK);
+			DrawLine(0, 400, 600, 400, BLACK);
+
+			if (latestMsg.find("YourMove") != std::string::npos || latestMsg.find("Waiting") != std::string::npos)
 			{
-				DrawCenteredText("Waiting for second player", 32);
-			}
-			else if (queryReceive.starts_with("StartIn"))
-			{
-				DrawCenteredText("Starting in " + queryReceive.substr(7), 32);
-			}
-			else if (queryReceive.find("Start") != std::string::npos)
-			{
-				while (true)
-				{
-					if (queryReceive.find("GameEnd") != std::string::npos) // idk why yet
+				std::string boardData = latestMsg.substr(latestMsg.find("\n") + 1);
+				for (int i = 0, k = 0; i < 3; i++)
+					for (int j = 0; j < 3; j++, k++)
+						board[i][j] = boardData[k + i];
+
+				for (int i = 0; i < 3; i++)
+					for (int j = 0; j < 3; j++)
 					{
-						DrawCenteredText("Game Ended", 32);
-						break;
+						if (board[i][j] != ' ')
+						{
+							char symbol[2] = { board[i][j], '\0' };
+							int fontSize = 80;
+							Vector2 textSize = MeasureTextEx(GetFontDefault(), symbol, (float)fontSize, 1);
+							float posX = j * 200 + (200 - textSize.x) / 2;
+							float posY = i * 200 + (200 - textSize.y) / 2;
+							DrawText(symbol, (int)posX, (int)posY, fontSize, BLUE);
+						}
 					}
+
+				if (latestMsg.find("YourMove") != std::string::npos && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+				{
+					Vector2 mouse = GetMousePosition();
+					int row = mouse.y / 200;
+					int col = mouse.x / 200;
+					std::string move = std::to_string(row) + std::to_string(col);
+					send(s, move.c_str(), (int)move.size(), 0);
 				}
 			}
 		}
 
 		EndDrawing();
 	}
+
+	closesocket(s);
+	WSACleanup();
 	CloseWindow();
+	return 0;
 }
